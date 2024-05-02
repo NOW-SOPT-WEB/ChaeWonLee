69f9b0708a50cf27bf58464fb89a223b6c675963
cindy-chaewon <chaewon1769@daum.net>
2024-05-02 16:17:33 +0900

    fix: 모달 css 수정

--- week3/src/components/Modal.jsx
+++ week3/src/components/Modal.jsx
@@ -8,7 +8,6 @@ const Modal = ({ onClose }) => {
     dialogRef.current.showModal();
   }, []);
 
-  // 모달 외부 클릭 이벤트 처리
   const handleOutsideClick = (event) => {
     if (event.target === dialogRef.current) {
       onClose();
@@ -18,7 +17,6 @@ const Modal = ({ onClose }) => {
 
   return (
     <ModalWrapper ref={dialogRef} onClick={handleOutsideClick}>
-      {/* 모달 컨텐트 대신 제목과 버튼을 여기 직접 포함 */}
       <ModalTitle>🥳축하해요!!!🥳</ModalTitle>
       <ModalButton
         onClick={() => {
@@ -45,14 +43,14 @@ const ModalWrapper = styled.dialog`
   left: 0;
   right: 0;
   bottom: 0;
-  z-index: 1;
 
   width: 30rem;
   height: 20rem;
   margin: auto;
   border: none;
   background-color: ${({ theme }) => theme.colors.white};
-  border-radius: 3rem; /* 모서리 둥글기 설정 */
+  border-radius: 3rem;
+
   &::backdrop {
     background-color: rgba(0, 0, 0, 0.5);
   }
@@ -68,7 +66,7 @@ const ModalButton = styled.button`
   width: 13rem;
   height: 4rem;
   font-size: 1.3rem;
-  margin-top: 2rem; /* 제목과의 간격 */
+  margin-top: 2rem;
   color: ${({ theme }) => theme.colors.light_yellow};
   background-color: ${({ theme }) => theme.colors.green};
   border: none;
